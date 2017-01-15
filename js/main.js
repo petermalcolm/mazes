abc25cbe39991769c8edac4ee2e330523d05d9fc
petermalcolm <peter.t.malcolm@gmail.com>
2017-01-15 14:18:42 -0500

    "extreme menorah-mode": directions now have weights

--- js/main.js
+++ js/main.js
@@ -14,10 +14,10 @@
 	const END_X = 8;
 	const END_Y = 0;
 
-	const deltas =[ {dir: 'n', del: { x: 0, y: -1 }, opposite: 's'},
-					{dir: 's', del: { x: 0, y:  1 }, opposite: 'n'},
-					{dir: 'e', del: { x: 1, y:  0 }, opposite: 'w'},
-					{dir: 'w', del: { x:-1, y:  0 }, opposite: 'e'}
+	const deltas =[ {dir: 'n', del: { x: 0, y: -1 }, weight: 1, opposite: 's'},
+					{dir: 's', del: { x: 0, y:  1 }, weight: 0, opposite: 'n'},
+					{dir: 'e', del: { x: 1, y:  0 }, weight: .3, opposite: 'w'},
+					{dir: 'w', del: { x:-1, y:  0 }, weight: .3, opposite: 'e'}
 				  ];
 	var main = function() {
 		var makeRow = function() {
@@ -30,7 +30,7 @@
 			var on_solution_path = false;
 			var nexts = [];
 			if( !stuck(x,y) ) {
-				var nexts = scramble(pickIfPassable(1,x,y));				
+				var nexts = prune(scramble(pickIfPassable(1,x,y)));
 			}
 			for( var direction in nexts ) {
 				on_solution_path = branchTo(x,y,nexts[direction]) || on_solution_path || madeIt(x,y);	
@@ -78,7 +78,7 @@
 				var direction = curVal.dir,
 					newX = x+curVal.del.x,
 					newY = y+curVal.del.y;
-				if('s' !== curVal.dir && Math.random() < likelihood && !notPassableCoords(newX,newY)) { 
+				if(Math.random() < likelihood && !notPassableCoords(newX,newY)) { 
 					return accum.concat(direction);
 				} else {
 					return accum;
@@ -89,6 +89,11 @@
 			return arr.reduce( function beforeOrAfter( accum, curVal, curIdx ){
 				return (Math.random() < .5) ? accum + curVal : curVal + accum;
 			},[]).split('');
+		};
+		var prune = function( arr ) {
+			return arr.reduce( function keepOrRemove( accum, curVal, curIdx ){
+				return (Math.random() < deltaOf(curVal).weight) ? accum + curVal : accum;
+			},[]);
 		};
 		var density = function() {
 			var ratio = { denom: $('.cell').length,
@@ -112,8 +117,8 @@
 		var branchTo = function( x,y,direction ) {
 			connectTwoCells( x,y,direction );
 			var coords = xyInDirection( x,y,direction );
-			if( madeIt( coords.x, coords.y ) ) {
-				// cellAt( coords.x, coords.y ).addClass('finish').html('End');
+			if( (coords.x===END_X && coords.y===END_Y) ) {
+				cellAt( coords.x, coords.y ).addClass('finish').html('End');
 			}
 			return madeIt( coords.x, coords.y ) || walkIt( coords.x, coords.y );
 		};
